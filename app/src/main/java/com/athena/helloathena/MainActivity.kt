6bed9f64cf47c4dfc16615e6e4b884ece2a28e3a
kof99athena <kof99athena@gmail.com>
2023-08-25 22:28:19 +0900

    Row예제, 오류해결 : @NoLiveLiterals

--- app/src/main/java/com/athena/helloathena/MainActivity.kt
+++ app/src/main/java/com/athena/helloathena/MainActivity.kt
@@ -9,8 +9,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,6 +23,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material.icons.materialIcon
@@ -32,6 +36,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MovableContent
+import androidx.compose.runtime.NoLiveLiterals
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
@@ -47,54 +52,68 @@ import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
 import com.athena.helloathena.ui.theme.HelloAthenaTheme
 
-class MainActivity : ComponentActivity() {
+
+@NoLiveLiterals
+class Row : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HelloAthenaTheme {
-                BoxEx()
-            }//HelloAthenaTheme
-        }//setContent
-    }//onCreate
-}//Activity class
-//1. 그냥 박스
-//2. frameLayout처럼 중첩용도로 쓰는경우
-@Composable
-//@Composable 주석은 UI 구성요소를 다루기 위해 쓴다.
-//내가만든 컴포저블 함수는 ModifierEx()이다
-fun BoxEx() {
-//    Box(modifier = Modifier.size(100.dp)) {
-//        Text(text = "Hello World", modifier = Modifier.align(Alignment.End))
-//    }
-
-//    Box(modifier = Modifier.size(100.dp)) {
-//        Text(text = "Hello World", modifier = Modifier.align(Alignment.BottomEnd))
-//        Text(text = "Jetpack", modifier = Modifier.align(Alignment.CenterEnd))
-//        Text(text = "Compose", modifier = Modifier.align(Alignment.TopStart))
-//    }
-
-//    Box(modifier = Modifier.size(100.dp)) {
-//        Box(modifier = Modifier.size(70.dp).background(Color.Cyan).align(Alignment.CenterStart))
-//        Box(modifier = Modifier.size(70.dp).background(Color.Blue).align(Alignment.BottomEnd))
-//
-//    }
-
-    Box {
-        Box(modifier = Modifier.matchParentSize().background(Color.Cyan).align(Alignment.CenterStart))
-        //부모 사이즈를 match로 하면 자식 사이즈에 맞춘다
-        Box(modifier = Modifier.size(70.dp).background(Color.Blue).align(Alignment.CenterStart))
-
+                RowEx()
+            }//athena theme
+        }
     }
+}
+@NoLiveLiterals
+@Composable
+fun RowEx() {
+//    Row(modifier = Modifier.height(40.dp)) {
+//        //Row는 가로 , align은 세로
+//        Text(text = "첫 번째!", modifier = Modifier.align(Alignment.Top))
+//        Text(text = "첫 번째!", modifier = Modifier.align(Alignment.CenterVertically))
+//        Text(text = "첫 번째!", modifier = Modifier.align(Alignment.Bottom))
+//    }
 
-
-
-}//ModifierEx method
-
+    Row(
+        //horizontalArrangement = Arrangement.SpaceAround//가로로 중앙정렬
+        verticalAlignment = Alignment.Bottom,
+        modifier = Modifier
+            .width(200.dp)
+            .height(40.dp)
+    ) {
+        //Row는 가로 , align은 세로
+        Text(
+            text = "첫 번째!",
+            modifier = Modifier
+                .align(Alignment.Top)
+                .weight(3f)
+                .background(Color.Magenta)
+        )
+        Icon(
+            imageVector = Icons.Filled.Add,
+            contentDescription = "추가"
+        )
+        Text(
+            text = "두 번째!",
+            modifier = Modifier.weight(3f) //weight은 비중
+        )
+        Icon(
+            imageVector = Icons.Filled.AccountBox,
+            contentDescription = "추가",
+            modifier = Modifier.weight(2f)
+        )
+        Text(
+            text = "세 번째!",
+            textAlign = TextAlign.Center,
+            modifier = Modifier.weight(3f)
+        )
+    }
+}
+@NoLiveLiterals
 @Preview(showBackground = true)
-//xml 파일이 아닌데도 프리뷰를 볼 수 있다. split으로 화면을 바로 볼수있다.
 @Composable
 fun GreetingPreview() {
     HelloAthenaTheme {
-        BoxEx()
+        RowEx()
     }
 }
