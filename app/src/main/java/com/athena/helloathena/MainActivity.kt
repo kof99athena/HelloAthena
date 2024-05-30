c7c2ced671e9278836d5cdae4e692b93dc9fb5d7
kof99athena <kof99athena@gmail.com>
2024-05-30 23:40:23 +0900

    Compose Button Test

--- app/src/main/java/com/athena/helloathena/MainActivity.kt
+++ app/src/main/java/com/athena/helloathena/MainActivity.kt
@@ -5,117 +5,70 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Send
-import androidx.compose.material.icons.materialIcon
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MovableContent
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
-import androidx.compose.ui.unit.sp
 import com.athena.helloathena.ui.theme.HelloAthenaTheme
 
-
 @NoLiveLiterals
-class Row : ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HelloAthenaTheme {
-                RowEx()
+                ButtonExample(onButtonClicked = {
+                    Toast.makeText(this, "Send Message", Toast.LENGTH_SHORT).show()
+                })
             }//athena theme
         }
     }
 }
-@NoLiveLiterals
+
 @Composable
-fun RowEx() {
-//    Row(modifier = Modifier.height(40.dp)) {
-//        //Row는 가로 , align은 세로
-//        Text(text = "첫 번째!", modifier = Modifier.align(Alignment.Top))
-//        Text(text = "첫 번째!", modifier = Modifier.align(Alignment.CenterVertically))
-//        Text(text = "첫 번째!", modifier = Modifier.align(Alignment.Bottom))
+fun ButtonExample(onButtonClicked: () -> Unit) {
+//    Button(onClick = onButtonClicked) {
+//        Text(text = "Send")
+//    }
+    //1. 버튼 클릭 시 토스트 띄우기
+//    Button(onClick = onButtonClicked) {
+//        Text(text = "Send")
 //    }
 
-    Row(
-        //horizontalArrangement = Arrangement.SpaceAround//가로로 중앙정렬
-        verticalAlignment = Alignment.Bottom,
-        modifier = Modifier
-            .width(300.dp)
-            .height(40.dp)
+    //2. Icon 추가하기
+    //modifier 추가하기
+    //버튼에 enabled 속성 추가
+    //shape 지정
+    Button(
+        onClick = onButtonClicked,
+        enabled = true,
+        border = BorderStroke(10.dp, Color.Magenta),
+        shape = RoundedCornerShape(10.dp),
+        contentPadding = PaddingValues(20.dp)
     ) {
-        //Row는 가로 , align은 세로
-        Text(
-            text = "첫 번째!",
-            modifier = Modifier
-                .align(Alignment.Top)
-                .weight(3f)
-                .background(Color.Magenta)
-        )
-        Icon(
-            imageVector = Icons.Filled.Add,
-            contentDescription = "추가"
-        )
-        Text(
-            text = "두 번째!",
-            modifier = Modifier.weight(3f) //weight은 비중
-                .background(Color.Yellow)
-        )
-        Icon(
-            imageVector = Icons.Filled.AccountBox,
-            contentDescription = "추가",
-            modifier = Modifier.weight(2f)
-        )
-        Text(
-            text = "세 번째!",
-            textAlign = TextAlign.Center,
-            modifier = Modifier.weight(3f)
-                .background(Color.Gray)
-        )
+        Icon(imageVector = Icons.Filled.Send, contentDescription = "Send message to user")
+        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+        Text(text = "Send")
     }
+
 }
-@NoLiveLiterals
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HelloAthenaTheme {
-        RowEx()
+        ButtonExample(onButtonClicked = {})
     }
 }
